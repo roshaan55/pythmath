5173940adfbc1268dfada6a257a212e1212014a6
Roshaan Mehmood <46756917+roshaan55@users.noreply.github.com>
2022-06-04 14:28:59 +0500

    Created all-trignometric-functions.py file

---
+++ Examples/Trignometric Functions/all-trignometric-functions.py
@@ -0,0 +1,21 @@
+from pythmath import *
+
+
+angle = 25
+print("Degrees to Radians:", deg_to_rad(angle))
+print("Radians to Degrees:", rad_to_deg(angle))
+print("sin(x):", sin(angle))
+print("sin(x) in degrees:", sind(angle))
+print("sinh(x):", sinh(angle))
+print("cos(x):", cos(angle))
+print("cos in degrees:", cosd(angle))
+print("cosh(x):", cosh(angle))
+print("tan(x):", tan(angle))
+print("tan(x) in degrees:", tand(angle))
+print("tanh(x):", tanh(angle))
+print("cot(x): ", cot(angle))
+print("cot(x) in degrees:", cotd(angle))
+print("sec(x):", sec(angle))
+print("sec(x) in degrees:", secd(angle))
+print("cosec(x):", cosec(angle))
+print("cosec(x) in degrees:", cosecd(angle))
