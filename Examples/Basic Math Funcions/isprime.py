7ca52c3a371aae01c334472070563cb52f321c8e
Roshaan Mehmood <46756917+roshaan55@users.noreply.github.com>
2022-06-04 13:15:42 +0500

    Created isprime.py file

---
+++ Examples/Basic Math Funcions/isprime.py
@@ -0,0 +1,11 @@
+import pythmath
+
+x = 7
+
+print(pythmath.isprime(x))  # returns True
+
+# Logical Implementation
+if pythmath.isprime(x):
+    print("Number", x, "is a prime number")
+else:
+    print("Number", x, "is not a prime number")
